e079e718b114d97542eb0e33c7962063b43d4eb4
Jason Kendall <coolacid@gmail.com>
2020-08-10 16:41:43 -0400

    Fix sign inversion

--- dugonFinder.py
+++ dugonFinder.py
@@ -16,10 +16,10 @@ class GalacticraftDung:
         chunkZ = int(chunkZ)
 
         if chunkX < 0:
-            chunkX -= numChunks - 1
+            chunkX = -numChunks
 
         if chunkZ < 0:
-            chunkZ -= numChunks - 1
+            chunkZ = -numChunks
 
         k = chunkX / numChunks
         l = chunkZ / numChunks
@@ -34,6 +34,13 @@ class GalacticraftDung:
 
         return k, l
 
+    def getChunkofDung(self, xpos, zpos, Dimention):
+        x = math.floor(xpos/16)
+        z = math.floor(zpos/16)
+        k, l = self.getDungeonPosForCoords(x, z, Dimention)
+        print("Got chunk [{}, {}]".format(k, l))
+
+
     def directionToNearestDungeon(self, xpos, zpos, Dimention):
         x = math.floor(xpos)
         z = math.floor(zpos)
@@ -43,15 +50,16 @@ class GalacticraftDung:
         searchOffsetZ = int(quadrantZ / (self.Spacing / 2))  # 0 or 1
         nearestX = 0
         nearestZ = 0
-        for cx in range(searchOffsetX - 1, searchOffsetX + 1):
-            for cz in range(searchOffsetZ - 1, searchOffsetZ + 1):
+        for cx in range(searchOffsetX - 1, searchOffsetX + 2): # Python range is not ending inclusive
+            for cz in range(searchOffsetZ - 1, searchOffsetZ + 2): # Python range is not ending inclusive
                 k, l = self.getDungeonPosForCoords((x + cx * self.Spacing) / 16, (z + cz * self.Spacing) / 16, Dimention)
-                print("Expected chunk [{}, {}]".format(k, l))
+                print("Got chunk [{}, {}]".format(k, l))
                 i = 2 + k
                 j = 2 + l
                 oX = i - xpos
                 oZ = j - zpos
                 distanceSq = oX * oX + oZ * oZ
+                print("X: {}, Z: {}, D: {}".format(oX, oZ, distanceSq))
                 if distanceSq < self.nearestDistance:
                     return distanceSq, oX, oZ
 
@@ -61,6 +69,7 @@ print("Location: 1327, -544")
 d,x,z = g.directionToNearestDungeon(1327, -544, -12)
 print("X: {}, Z: {}, D: {}".format(x, z, d))
 print("dat file shows (Chunk): [78,-31], which is: 1248, -496")
+#g.getChunkofDung(1327, -544, -12)
 
 #print("Location: 1428, -639")
 #d,x,z = g.directionToNearestDungeon(1428, -639, -14)
